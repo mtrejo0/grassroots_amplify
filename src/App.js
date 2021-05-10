e44c8d95ff64af3046fe2e7ea0d150f147b5a5c1
Moises Trejo <moisest@mit.edu>
2021-05-10 13:18:39 -0500

    added modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+import Home from './containers/home'
+import About from './containers/about'
+import RepPage from './components/rep_page'
+
+import NavigationBar from './components/nav_bar'
+
+import { Route, Redirect, Switch, HashRouter} from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      <HashRouter>
+        <NavigationBar></NavigationBar>
+        <Switch>
+          <Route exact path='/' render={() =>(
+            <Redirect to='/home'/>
+          )}/>
+          <Route path='/home' component={Home}></Route>
+          <Route path='/about' component={About}></Route>
+          <Route path='/rep/:name' component={RepPage}></Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
